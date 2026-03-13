84b9fae55bc6db000a8ffe62d2aaa3649abf6e45
ANDREY LUIZ DA ROSA REBELATTO <andrey.rebelatto@univille.br>
2026-03-13 20:04:25 -0300

    add: create aula construtores

---
+++ aula-construtores/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Retangulo retangulo = new Retangulo();
+
+        Retangulo retangulo2 = new Retangulo(5,5);
+
+        System.out.println(retangulo2);
+        System.out.println(retangulo2.getAltura());
+    }
+}
